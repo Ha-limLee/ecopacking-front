266c4bdac89a5c2dce1a8ca3a528c9f8dcabb38e
LeeHalim <leehalim09@gmail.com>
2022-08-21 02:52:13 +0900

    added feature/input-order-number

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,22 +6,22 @@ import Carousel from 'nuka-carousel';
 import PackingBox from 'types/PackingBox';
 
 export default function App() {
-  const [data, setData] = React.useState<PackingBox[]>([]);
-  const [orderNumber, setOrderNumber] = React.useState(0);
+  const [data, setData] = React.useState<PackingBox[]>([{size: '0', ppList: []}]);
+  const [orderNumber, setOrderNumber] = React.useState('');
 
   React.useEffect(() => {
-    fetch('/eco/order/1')
+    fetch('/eco/order/' + orderNumber)
       .then((val) => val.json())
       .then(x => {
         console.log(x.data);
         setData(x.data);
       });
-  }, []);
+  }, [orderNumber]);
 
   return (
     <div id='App'>
       <Carousel>
-        {data.map(x => <AppView packingBox={x} />)}
+        {data.map(x => <AppView packingBox={x} onSearch={setOrderNumber}/>)}
       </Carousel>
     </div>
   );
