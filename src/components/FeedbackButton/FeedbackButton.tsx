3b7f9be212cbd870d5be7a60927bc05b653911e1
LeeHalim <leehalim09@gmail.com>
2022-08-21 16:26:15 +0900

    adjusted FeedbackRadio size

--- src/components/FeedbackButton/FeedbackButton.tsx
+++ src/components/FeedbackButton/FeedbackButton.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import FeedbackButtonView from './FeedbackButton.view';
 
-export default function FeedbackButton({text}: {text: string, modalText: string}) {
+/**
+ * materialCount := 포장재를 사용하는 물품 개수
+ */
+export default function FeedbackButton({text, materialCount}: {text: string, materialCount: number}) {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-  
 
     return (
-      <FeedbackButtonView text={text} isOpen={open} materialCount={5} handleOpen={handleOpen} handleClose={handleClose}/>
+      <FeedbackButtonView text={text} isOpen={open} materialCount={materialCount} handleOpen={handleOpen} handleClose={handleClose}/>
     );
 }
