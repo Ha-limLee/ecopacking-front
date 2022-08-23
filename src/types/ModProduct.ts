98aa87f4ddcfc464af28678162608a4c55d3483f
LeeHalim <leehalim09@gmail.com>
2022-08-24 03:24:37 +0900

    group same items

---
+++ src/types/ModProduct.ts
@@ -0,0 +1,11 @@
+import PackingProduct from "./PackingProduct";
+
+type ModProduct = {
+    id: string;
+    name: string;
+    material: string;
+    length: string;
+    count: number;
+};
+
+export default ModProduct;
